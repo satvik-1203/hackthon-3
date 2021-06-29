048e99b8e51c80d9c926af303128553781ed789a
satvik-1203 <satvikgsu@gmail.com>
2021-06-29 23:26:45 +0530

    hashed

--- src/functions/createAccount.ts
+++ src/functions/createAccount.ts
@@ -2,36 +2,46 @@ import readline from "readline-sync";
 import { IUser } from "../interface";
 import { readDB } from "../misc";
 import { readFile, writeFile } from "fs/promises";
+import bcrypt from "bcrypt";
 
 const createAccount = async (): Promise<void> => {
-  const url = "database/Users.json";
-  const labels = ["Name", "Email", "Password"];
-  const userDetails: string[] = [];
-  while (userDetails.length < labels.length) {
-    const answer = readline
-      .question(`Enter your ${labels[userDetails.length]}: `)
-      .trim();
-    if (answer) userDetails.push(answer);
-  }
-  const [name, email, password] = userDetails;
-  const userInfo: IUser = {
-    name,
-    email,
-    password,
-    todo: [],
-  };
+  try {
+    const url = "database/Users.json";
+    const labels = ["Name", "Email", "Password"];
+    const userDetails: string[] = [];
+    while (userDetails.length < labels.length) {
+      const answer = readline
+        .question(`Enter your ${labels[userDetails.length]}: `)
+        .trim();
+      if (answer) userDetails.push(answer);
+    }
+    let [name, email, password] = userDetails;
 
-  const Users = await readDB();
-  if (!Users) return;
-  const exist = Users.find((user) => user.email === email);
-  if (exist) {
-    console.log("Email already exist, Please try again...");
-    createAccount();
-    return;
-  }
-  Users.push(userInfo);
+    password = await bcrypt.hash(password, 10);
 
-  await writeFile(url, JSON.stringify(Users)).catch((err) => console.log(err));
+    const userInfo: IUser = {
+      name,
+      email,
+      password,
+      todo: [],
+    };
+
+    const Users = await readDB();
+    if (!Users) return;
+    const exist = Users.find((user) => user.email === email);
+    if (exist) {
+      console.log("Email already exist, Please try again...");
+      createAccount();
+      return;
+    }
+    Users.push(userInfo);
+
+    await writeFile(url, JSON.stringify(Users)).catch((err) =>
+      console.log(err)
+    );
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export default createAccount;
